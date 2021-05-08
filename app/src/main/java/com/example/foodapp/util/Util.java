c171922b3030bf17d51fd150d86e672de92e909b
makara_chhaing <makara.chhaing@gmail.com>
2021-05-08 14:45:49 +1000

    create Util

---
+++ app/src/main/java/com/example/foodapp/util/Util.java
@@ -0,0 +1,19 @@
+package com.example.foodapp.util;
+
+import com.example.foodapp.databasehelper.Database;
+
+public class Util {
+    public  static Database db;
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "users_db";
+
+    public static final String TABLE_NAME = "users_table";
+    public static final String USER_ID = "user_id";
+    public static final String USERNAME = "user_name";
+    public static final String PASSWORD = "password";
+    public static final String EMAIL = "email";
+    public static final String PHONE = "phone";
+    public static final String ADDRESS = "address";
+    public static final String NAME = "name";
+    public static final String DEBUG = "databaseCreate1";
+}
